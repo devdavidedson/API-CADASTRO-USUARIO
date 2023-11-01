219f8a0378df5d2028773c1a51c8cc910a1d4e2c
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-11-01 13:33:02 -0300

    organizando GET e POST do service

--- Backend/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ Backend/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,32 +1,35 @@
 package br.com.criandoapi.projeto.controller;
 
-import br.com.criandoapi.projeto.DAO.IUsuario;
+import br.com.criandoapi.projeto.repository.IUsuario;
 import br.com.criandoapi.projeto.model.Usuario;
+import br.com.criandoapi.projeto.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
+
     @Autowired
     private IUsuario dao;
+    private UsuarioService usuarioService;
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> listaDeUsuarios() {
-     List<Usuario> lista = (List<Usuario>) dao.findAll();
-     return ResponseEntity.status(200).body(lista);
+    public ResponseEntity<List<Usuario>> listaUsuarios() {
+     return ResponseEntity.status(200).body(usuarioService.listarUsuario());
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> criarUsuarios(@RequestBody Usuario usuario) {
-        Usuario usuarioNovo = dao.save(usuario);
-        return ResponseEntity.status(201).body(usuarioNovo);
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
     }
 
     @PutMapping
